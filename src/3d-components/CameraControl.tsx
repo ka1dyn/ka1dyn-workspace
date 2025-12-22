6ca1352a8b1d860d2ec19997ab79fcbf43537af7
ka1dyn <wt0630@naver.com>
2025-12-23 04:37:27 +0900

    💄 Fix camera control

--- src/3d-components/CameraControl.tsx
+++ src/3d-components/CameraControl.tsx
@@ -1,16 +1,20 @@
 import { folder, useControls } from "leva"
 import { useCameraInit, useStart } from "@/stores"
 import { useShallow } from "zustand/shallow"
-import { useCallback, useEffect, useRef } from "react"
+import { useCallback, useEffect, useRef, useState } from "react"
 import { OrbitControls, PerspectiveCamera } from "@react-three/drei"
 import { degToRad } from "three/src/math/MathUtils.js"
 import * as THREE from 'three'
 import gsap from "gsap"
-import { GsapEase } from "@/enums"
+import { GsapEase } from "@/types/enums"
+import type { OrbitControls as OrbitControlsImpl } from 'three-stdlib'
+
 
 
 export default function CameraControl() {
     const cameraRef = useRef<THREE.PerspectiveCamera>(null!);
+    const controlRef = useRef<OrbitControlsImpl>(null!);
+    const [nowAnimating, setNowAnimation] = useState<boolean>(false);
 
     // Set Camera init position
     const {target, pos} = useCameraInit(useShallow((state) => ({
@@ -34,7 +38,9 @@ export default function CameraControl() {
         })
     }))
 
+    // Init camera setting
     useEffect(() => {
+        // Leva
         set({
             tx: target.x,
             ty: target.y,
@@ -45,31 +51,52 @@ export default function CameraControl() {
         })
     }, [target, pos, set])
 
-    // Set start animation
+    // Set init animation
     useEffect(() => {
         if (!start) return;
-
+        setNowAnimation(true);
         startAnimation()
 
     }, [start])
 
     const startAnimation = useCallback(() => {
-        gsap.to(cameraRef.current.position, {
+        const tl = gsap.timeline({
+            onComplete: () => {
+                setNowAnimation(false);
+            }
+        })
+
+        tl.to(cameraRef.current.position, {
             x: 7.5,
             y: 2,
             z: 6,
-            duration: 5,
+            duration: 4,
             ease: GsapEase.EXPO_IN,
-            delay: 2
+            delay: 2,
         })
-    }, [cameraRef])
+        tl.to(controlRef.current.target, {
+            x: 0,
+            y: 1,
+            z: 0,
+            duration: 4,
+            ease: GsapEase.EXPO_IN,
+            delay: 2,
+            onUpdate: () => {
+                controlRef.current.update();
+            },
+        }, 0)
+    }, [cameraRef, controlRef])
 
     return <>
         <OrbitControls
+            ref={controlRef}
             makeDefault
             target={[tx, ty, tz]}
             maxPolarAngle={degToRad(89.5)}
             dampingFactor={0.05}
+            maxDistance={10}
+            enablePan={false}
+            enabled={!nowAnimating}
         />
         <PerspectiveCamera ref={cameraRef} near={0.01} far={50} fov={50} position={[px, py, pz]} makeDefault />
     </>
