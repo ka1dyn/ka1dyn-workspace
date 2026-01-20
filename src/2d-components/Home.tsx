8fbc8a91f76069ceb0ed6af54ad00e2a9fcfe098
ka1dyn <wt0630@naver.com>
2026-01-20 09:42:07 +0900

    ✨ Add header drop filter blur

--- src/2d-components/Home.tsx
+++ src/2d-components/Home.tsx
@@ -21,9 +21,14 @@ export default function Home() {
     <div className="w-full h-full relative overflow-hidden bg-black">
       <img
         src="/images/happy_dog.jpg"
-        className="w-full h-full object-cover object-[50%] opacity-40"
+        className="absolute w-full h-full object-cover object-[50%] opacity-60"
       />
-      <div className="w-full h-10 bg-gray-200"></div>
+      <div
+        className="w-full h-16 relative overflow-hidden
+                bg-gray-300/30 backdrop-blur-xl
+                border-b border-black/20
+                shadow-md"
+      ></div>
       <div className="flex flex-col gap-10 w-24 min-h-50 absolute right-15 top-20">
         <div className="flex flex-col items-center gap-2">
           <div className="w-full h-24 bg-amber-500 text-center"></div>
