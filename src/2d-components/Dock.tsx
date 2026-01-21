ea7a939897a15bd14c7fa0bf9204ad48ce1d7cc1
ka1dyn <wt0630@naver.com>
2026-01-22 07:01:54 +0900

    ✨ Add dock animation

--- src/2d-components/Dock.tsx
+++ src/2d-components/Dock.tsx
@@ -1,6 +1,6 @@
 import { cn } from "@/lib/utils";
 import { BackgroundTypes } from "@/types/enums";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 
 export default function Dock({
   className,
@@ -9,46 +9,31 @@ export default function Dock({
   className?: string;
   setImgSrc: (path: string) => void;
 }) {
+  const thumnailRefs = useRef<(HTMLDivElement | null)[]>([]);
   const [bgType, setBgType] = useState<BackgroundTypes>(BackgroundTypes.DOG);
-  const indicatorRefs = useRef<(HTMLDivElement | null)[]>([]);
-  const prevRef = useRef<HTMLDivElement>(null!);
+  const [indicatorOffset, setIndicatorOffset] = useState<number>(0);
 
   const clickThumnail = (type: BackgroundTypes) => {
-    // Indicator move
-    const curDiv = indicatorRefs.current[type] as HTMLDivElement;
-
-    curDiv.style.backgroundColor = "gray";
-    console.log(curDiv.style.backgroundColor);
-
-    if (type === BackgroundTypes.BIKE) {
-      setBgType(type);
-    }
+    setBgType(type);
   };
 
-  useEffect(() => {
-    if (!prevRef.current) {
-      prevRef.current = indicatorRefs.current[bgType] as HTMLDivElement;
-      return;
-    }
+  useLayoutEffect(() => {
+    const curThumnail = thumnailRefs.current[bgType];
+    if (!curThumnail) return;
 
+    setIndicatorOffset(curThumnail.offsetLeft);
+  }, [bgType]);
+
+  useEffect(() => {
     switch (bgType) {
       case BackgroundTypes.DOG:
         setImgSrc("/images/happy_dog.webp");
-        prevRef.current = indicatorRefs.current[
-          BackgroundTypes.DOG
-        ] as HTMLDivElement;
         break;
       case BackgroundTypes.BIKE:
         setImgSrc("/images/bike_rain.webp");
-        prevRef.current = indicatorRefs.current[
-          BackgroundTypes.BIKE
-        ] as HTMLDivElement;
         break;
       case BackgroundTypes.CITY:
         setImgSrc("/images/city_rain.webp");
-        prevRef.current = indicatorRefs.current[
-          BackgroundTypes.CITY
-        ] as HTMLDivElement;
         break;
       default:
         return;
@@ -64,40 +49,33 @@ export default function Dock({
     >
       <div className="w-full h-full flex gap-6 justify-center items-center -translate-y-1">
         <div
+          ref={(node) => {
+            thumnailRefs.current[0] = node;
+          }}
           className="relative cursor-pointer size-16 bg-amber-100 rounded-xl bg-[url(/images/happy_dog_thumbnail.jpg)] bg-center bg-no-repeat bg-cover hover:shadow-lg/50 shadow-white"
           onClick={() => clickThumnail(BackgroundTypes.DOG)}
-        >
-          <div
-            style={{ backgroundColor: "black" }}
-            ref={(el) => {
-              indicatorRefs.current[0] = el;
-            }}
-            className="absolute -bottom-2.75 left-1/2 -translate-x-1/2 size-1.5 rounded-full bg-black"
-          ></div>
-        </div>
+        ></div>
         <div
+          ref={(node) => {
+            thumnailRefs.current[1] = node;
+          }}
           className="relative cursor-pointer size-16 bg-amber-100 rounded-xl bg-[url(/images/bike_rain_thumbnail.jpg)] bg-center bg-no-repeat bg-cover hover:shadow-lg/50 shadow-white"
           onClick={() => clickThumnail(BackgroundTypes.BIKE)}
-        >
-          <div
-            ref={(el) => {
-              indicatorRefs.current[1] = el;
-            }}
-            className="absolute -bottom-2.75 left-1/2 -translate-x-1/2 size-1.5 rounded-full bg-black"
-          ></div>
-        </div>
+        ></div>
         <div
+          ref={(node) => {
+            thumnailRefs.current[2] = node;
+          }}
           className="relative cursor-pointer size-16 bg-amber-100 rounded-xl bg-[url(/images/city_rain_thumbnail.jpg)] bg-center bg-no-repeat bg-cover hover:shadow-lg/50 shadow-white"
           onClick={() => clickThumnail(BackgroundTypes.CITY)}
-        >
-          <div
-            ref={(el) => {
-              indicatorRefs.current[2] = el;
-            }}
-            className="absolute -bottom-2.75 left-1/2 -translate-x-1/2 size-1.5 rounded-full bg-black"
-          ></div>
-        </div>
+        ></div>
         <div className="absolute left-0 bottom-0 "></div>
+        <div
+          className={`absolute bottom-0 left-8 size-1.5 rounded-full bg-white transition-all duration-400 ease-in-out`}
+          style={{
+            transform: `translateX(calc(${indicatorOffset}px - 50%))`,
+          }}
+        ></div>
       </div>
     </div>
   );
